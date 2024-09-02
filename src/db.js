cb7c16f024a65020f36cac930b611f099ec5441a
Nyfong <litongfong12@gmail.com>
2024-09-02 22:20:27 +0700

    first commit

---
+++ src/db.js
@@ -0,0 +1,56 @@
+const dbNews = [
+  {
+    title: `
+    <span class="font-bold">THAILAND:</span>
+    Paetongtarn, the 31st Prime Minister of Thailand Politics 16 Aug
+    2024 LISTEN soundwave Pheu Thai leader Paetongtarn Shinawatra
+    was elected by parliament as the 31st prime minister of
+    Thailand, becoming the third member of the Shinawatra family to
+    hold the top job, following her father, Thaksin, and her aunt,
+    Yingluck. Paetongtarn succeeds Srettha Thavisin, who was ousted
+    by the Constitutional Court this week. She was the only
+    candidate nominated for the position. She won with 319 votes in
+    favour, 145 votes against and 27 abstentions. She needed at
+    least 248 votes to win. Nicknamed “Ung Ing”, Paetongtarn, 37,
+    becomes the youngest prime minister of Thailand and the second
+    female. She is the third child of Thaksin Shinawatra and
+    Khunying Potjaman Na Pombejra. Paetongtarn gave birth to her
+    second child during the general election campaign in May last
+    year. Paetongtarn was not in Parliament today for her
+    nomination, because she is not an MP. Reports indicate that she
+    is following developments from the former Voice TV station
+    facility. Her victory was assured, though, as the leaders of the
+    nine government coalition parties appeared yesterday at a press
+    conference, hosted by Pheu Thai, to announce her nomination.
+  </span>`,
+  },
+  {
+    title: `
+    <span class="font-bold">THAILAND:</span>
+    Paetongtarn, the 31st Prime Minister of Thailand Politics 16 Aug
+    2024 LISTEN soundwave Pheu Thai leader Paetongtarn Shinawatra
+    was elected by parliament as the 31st prime minister of
+    Thailand, becoming the third member of the Shinawatra family to
+    hold the top job, following her father, Thaksin, and her aunt,
+    Yingluck. Paetongtarn succeeds Srettha Thavisin, who was ousted
+    by the Constitutional Court this week. She was the only
+    candidate nominated for the position. She won with 319 votes in
+    favour, 145 votes against and 27 abstentions. She needed at
+    least 248 votes to win. Nicknamed “Ung Ing”, Paetongtarn, 37,
+    becomes the youngest prime minister of Thailand and the second
+    female. She is the third child of Thaksin Shinawatra and
+    Khunying Potjaman Na Pombejra. Paetongtarn gave birth to her
+    second child during the general election campaign in May last
+    year. Paetongtarn was not in Parliament today for her
+    nomination, because she is not an MP. Reports indicate that she
+    is following developments from the former Voice TV station
+    facility. Her victory was assured, though, as the leaders of the
+    nine government coalition parties appeared yesterday at a press
+    conference, hosted by Pheu Thai, to announce her nomination.
+  </span>`,
+  },
+];
+
+dbNews.forEach((el) => {
+  console.log(el.title);
+});
